7699e14978bf1af84110b7e8d1668146f33a46c6
LingMyat223 <lingmyataung@outlook.com>
2023-02-03 17:02:17 +0630

    :wink: express socket.io for realtime

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+
+const app = express();
+
+const server = require('http').createServer(app);
+
+const io = require('socket.io')(server,{
+    cors : {origin : "*"}
+});
+
+let users = [];
+
+io.on('connection',(socket)=>{
+    console.log('Someone is connect');
+    socket.on('joinRoom',(joinRoomData)=>{
+        socket.join(joinRoomData.roomId);
+        users.push({
+            id : socket.id,
+            name : joinRoomData.name,
+            profile : joinRoomData.profile,
+            roomId : joinRoomData.roomId
+        });
+        socket.on('message',(data)=>{
+            io.sockets.to(joinRoomData.roomId).emit('message',data);
+        });
+
+        socket.on('image',(data)=>{
+            io.sockets.to(joinRoomData.roomId).emit('image',data);
+        });
+
+        socket.broadcast.to(joinRoomData.roomId).emit('joining',joinRoomData.name);
+    })
+
+
+    socket.on('disconnect',()=>{
+        const user = users.find(e=>e.id == socket.id);
+        if(user){
+            let index = users.findIndex(e=>e.id == user.id);
+            users.splice(index,1);
+            io.sockets.to(user.roomId).emit('leaving',user.name);
+        }else {
+            console.log('not in room');
+        }
+    })
+});
+
+server.listen(3000);
